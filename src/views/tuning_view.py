30f375fc96d9e1d4c5d757ee7e2009a29458c4d8
jesga06 <jessesouzaamorim@gmail.com>
2026-07-24 15:39:30 -0300

    fix(ui): fix process_analog_stick parameter swap and update reset deadzone default to 0 in TuningView

--- src/views/tuning_view.py
+++ src/views/tuning_view.py
@@ -624,7 +624,7 @@ class TuningView(QWidget):
 
     def reset_stick_defaults(self, section):
         if section == "analog_left":
-            self.slider_l_dz.setValue(5)
+            self.slider_l_dz.setValue(0)
             self.slider_l_adz.setValue(0)
             self.slider_l_rdz.setValue(0)
             self.slider_l_warp.setValue(100)
@@ -632,7 +632,7 @@ class TuningView(QWidget):
             self.slider_l_sens.setValue(10)
             self.combo_l_ctype.setCurrentText("linear")
         else:
-            self.slider_r_dz.setValue(5)
+            self.slider_r_dz.setValue(0)
             self.slider_r_adz.setValue(0)
             self.slider_r_rdz.setValue(0)
             self.slider_r_warp.setValue(100)
@@ -641,97 +641,22 @@ class TuningView(QWidget):
             self.combo_r_ctype.setCurrentText("linear")
         QMessageBox.information(self, "Reset Defaults", f"✓ Stick parameters reset to defaults for {section}.")
 
-    # ---------------------------------------------------------------------
-    # Right Stick Event Handlers
-    # ---------------------------------------------------------------------
-    def on_r_dz_changed(self, v):
-        val = v / 100.0
-        self.lbl_val_r_dz.setText(f"{val:.2f}")
-        self.radar_right.set_deadzone(v)
-        self.save_opt("analog_right", "deadzone", val)
-
-    def on_r_adz_changed(self, v):
-        val = v / 100.0
-        self.lbl_val_r_adz.setText(f"{val:.2f}")
-        self.save_opt("analog_right", "anti_deadzone", val)
-
-    def on_r_rdz_changed(self, v):
-        val = v / 100.0
-        self.lbl_val_r_rdz.setText(f"{val:.2f}")
-        self.save_opt("analog_right", "rest_deadzone", val)
-
-    def on_r_warp_changed(self, v):
-        val = v / 100.0
-        self.lbl_val_r_warp.setText(f"{val:.2f}")
-        self.save_opt("analog_right", "outer_max", val)
-
-    def on_r_cf_changed(self, v):
-        val = v / 10.0
-        self.lbl_val_r_cf.setText(f"{val:.2f}")
-        self.curve_graph_right.set_curve_params(self.combo_r_ctype.currentText(), val, self.edit_r_custom_eq.text())
-        self.save_opt("analog_right", "exp_factor", val)
-
-    def on_r_sens_changed(self, v):
-        val = v / 10.0
-        self.lbl_val_r_sens.setText(f"{val:.2f}")
-        self.save_opt("analog_right", "sensitivity", val)
-
-    def on_r_ctype_changed(self, ctype):
-        is_custom = (ctype == "custom")
-        self.edit_r_custom_eq.setVisible(is_custom)
-        self.curve_graph_right.set_curve_params(ctype, self.slider_r_cf.value() / 10.0, self.edit_r_custom_eq.text())
-        self.save_opt("analog_right", "curve", ctype)
-
-    def on_r_custom_eq_changed(self, text):
-        self.curve_graph_right.set_curve_params(self.combo_r_ctype.currentText(), self.slider_r_cf.value() / 10.0, text)
-
-    # ---------------------------------------------------------------------
-    # Left Trigger Event Handlers
-    # ---------------------------------------------------------------------
-    def on_lt_dz_changed(self, v):
-        val = v / 100.0
-        self.lbl_val_lt_dz.setText(f"{val:.2f}")
-        self.curve_graph_lt.set_curve_params(self.combo_lt_ctype.currentText(), self.slider_lt_exp.value() / 10.0)
-        self.save_opt("trigger_left", "deadzone", val)
-
-    def on_lt_adz_changed(self, v):
-        val = v / 100.0
-        self.lbl_val_lt_adz.setText(f"{val:.2f}")
-        self.curve_graph_lt.set_curve_params(self.combo_lt_ctype.currentText(), self.slider_lt_exp.value() / 10.0)
-        self.save_opt("trigger_left", "anti_deadzone", val)
-
-    def on_lt_rdz_changed(self, v):
-        val = v / 100.0
-        self.lbl_val_lt_rdz.setText(f"{val:.2f}")
-        self.curve_graph_lt.set_curve_params(self.combo_lt_ctype.currentText(), self.slider_lt_exp.value() / 10.0)
-        self.save_opt("trigger_left", "rest_deadzone", val)
-
-    def on_lt_exp_changed(self, v):
-        val = v / 10.0
-        self.lbl_val_lt_exp.setText(f"{val:.2f}")
-        self.curve_graph_lt.set_curve_params(self.combo_lt_ctype.currentText(), val)
-        self.save_opt("trigger_left", "exp_factor", val)
-
-    def on_lt_sens_changed(self, v):
-        val = v / 10.0
-        self.lbl_val_lt_sens.setText(f"{val:.2f}")
-        self.curve_graph_lt.set_curve_params(self.combo_lt_ctype.currentText(), self.slider_lt_exp.value() / 10.0)
-        self.save_opt("trigger_left", "sensitivity", val)
-
-    def on_lt_ctype_changed(self, ctype):
-        self.curve_graph_lt.set_curve_params(ctype, self.slider_lt_exp.value() / 10.0)
-        self.save_opt("trigger_left", "curve", ctype)
-
     def reset_trigger_defaults(self, section):
         if section == "trigger_left":
-            self.slider_lt_dz.setValue(5)
+            self.slider_lt_dz.setValue(0)
             self.slider_lt_adz.setValue(0)
             self.slider_lt_rdz.setValue(0)
             self.slider_lt_exp.setValue(10)
             self.slider_lt_sens.setValue(10)
             self.combo_lt_ctype.setCurrentText("linear")
         else:
-            self.slider_rt_dz.setValue(5)
+            self.slider_rt_dz.setValue(0)
+            self.slider_rt_adz.setValue(0)
+            self.slider_rt_rdz.setValue(0)
+            self.slider_rt_exp.setValue(10)
+            self.slider_rt_sens.setValue(10)
+            self.combo_rt_ctype.setCurrentText("linear")
+        QMessageBox.information(self, "Reset Defaults", f"✓ Trigger parameters reset to defaults for {section}.")
             self.slider_rt_adz.setValue(0)
             self.slider_rt_rdz.setValue(0)
             self.slider_rt_exp.setValue(10)
@@ -910,24 +835,28 @@ class TuningView(QWidget):
         # Compute tuned stick outputs using math_utils
         dz_l = self.slider_l_dz.value() / 100.0
         adz_l = self.slider_l_adz.value() / 100.0
+        rdz_l = self.slider_l_rdz.value() / 100.0
         warp_l = self.slider_l_warp.value() / 100.0
         ctype_l = self.combo_l_ctype.currentText()
         cf_l = self.slider_l_cf.value() / 10.0
+        sens_l = self.slider_l_sens.value() / 10.0
         custom_l = self.edit_l_custom_eq.text()
 
         mod_lx, mod_ly = math_utils.process_analog_stick(
-            lx, ly, dz_l, adz_l, warp_l, ctype_l, cf_l, custom_l
+            lx, ly, dz_l, adz_l, ctype_l, cf_l, rest_dz=rdz_l, sensitivity=sens_l, custom_eq=custom_l
         )
 
         dz_r = self.slider_r_dz.value() / 100.0
         adz_r = self.slider_r_adz.value() / 100.0
+        rdz_r = self.slider_r_rdz.value() / 100.0
         warp_r = self.slider_r_warp.value() / 100.0
         ctype_r = self.combo_r_ctype.currentText()
         cf_r = self.slider_r_cf.value() / 10.0
+        sens_r = self.slider_r_sens.value() / 10.0
         custom_r = self.edit_r_custom_eq.text()
 
         mod_rx, mod_ry = math_utils.process_analog_stick(
-            rx, ry, dz_r, adz_r, warp_r, ctype_r, cf_r, custom_r
+            rx, ry, dz_r, adz_r, ctype_r, cf_r, rest_dz=rdz_r, sensitivity=sens_r, custom_eq=custom_r
         )
 
         dz_lt = self.slider_lt_dz.value() / 100.0
